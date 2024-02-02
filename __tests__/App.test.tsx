bb4b59a5298203015d5826239234726da5acd9dc
dconco <onipededavid208@gmail.com>
2024-02-02 15:07:20 +0100

    Fix test function signature

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -12,6 +12,6 @@ import {it} from '@jest/globals';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
-it('renders correctly', () => {
+it('renders correctly', ():void => {
   renderer.create(<App />);
 });
